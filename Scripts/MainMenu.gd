18b102277bf94fff2971dcd716f466f4224fb1dd
CDiggityDawg0 <64213148+CDiggityDawg0@users.noreply.github.com>
2026-02-25 11:32:09 -0500

    Main menu and card cosmetics
    idk im scared

---
+++ Scripts/MainMenu.gd
@@ -0,0 +1,16 @@
+extends Node2D
+	
+func _on_singleplayer_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Game.tscn")
+
+	
+func _on_multiplayer_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Game.tscn")
+
+
+func _on_tutorial_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Game.tscn")
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
